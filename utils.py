f989236bad0116f329c961ee5085b90b5100e649
James Edmond <edmondandy795@gmail.com>
2023-10-07 09:44:06 -0400

    Made utils containing read_year, which will load the downloaded .csv filefrom a folder with path given as param

---
+++ utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+import pandas as pd
+import os
+
+
+
+def read_year(data_folder, year_int):
+    file_name = [ elem for elem in os.listdir(data_folder) if str(year_int) in elem ][0]
+    file_path = os.path.join(data_folder, file_name)
+    data = pd.read_csv(file_path,
+                       delimiter = ',',
+                       parse_dates=[0],
+                       infer_datetime_format=True, na_values='0',
+                       header = None)
+    return data
