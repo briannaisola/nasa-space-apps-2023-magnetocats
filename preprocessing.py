371382004f6b847f1e03d09518a4f4ec201bbd49
James Edmond <edmondandy795@gmail.com>
2023-10-08 14:18:43 -0400

    Made preprocessing file for handling removal of high-nan-count features in the FC variables and for selecting across k-length consecutive blocks of FC vars

---
+++ preprocessing.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+
+
+
+class dscovr_preprocessor:
+    """
+    does preprocessing for faraday cup vars; call like
+    dp_inst = dp()
+    subset = dp.drop_nan_threshold(data, threshold=0.3)
+    subset = dp.select_across_k_cups(subset, k=5)
+    """
+    
+    def __init__(self):
+        pass
+    
+    
+    def get_fc_vars(self, data):
+        return [ elem for elem in list(data) if 'FC' in elem ]
+    
+    
+    
+    def drop_nan_threshold(self, data, threshold=None):
+        if threshold is None: threshold = 0.2
+        
+        fc_vars = self.get_fc_vars(self.dat)
+        fc_dat = self.dat[fc_vars]
+
+        feature_nan_fracs = fc_dat.isna().sum(axis=0) / fc_dat.shape[0]
+        print('nan dist:',feature_nan_fracs)
+        
+        sort_inds = np.argsort(feature_nan_fracs).values
+        feat_inds_to_drop = sort_inds[::-1][ :int( sort_inds.shape[0] * threshold ) ]
+        feats_to_drop = np.array(fc_vars)[feat_inds_to_drop]
+        print('dropped',len(feats_to_drop),'features:',feats_to_drop)
+        
+        # extract vars that weren't dropped ...
+        vars_to_keep = np.setdiff1d( fc_vars, feats_to_drop ).tolist()
+        # ... and make sure that they're in the original FC sorted order!
+        vars_to_keep = np.array(vars_to_keep)[ np.argsort( [ int(elem[2:]) for elem in vars_to_keep ] ) ]
+        return fc_dat[vars_to_keep]
+    
+    
+    
+    def select_across_k_cups(self, data, k=None):
+        if k is None: k = 5
+        
+        # get fc vars for data provided
+        fc_vars = self.get_fc_vars(data)
+        
+        # keep only FC vars for those in blocks of length k - ignore remainder
+        feat_inds_to_keep = len(fc_vars) - (len(fc_vars) % k) + 1
+        feats_to_keep_k_blocks = np.array(fc_vars)[ :feat_inds_to_keep ].tolist()
+        
+        # extract first feature of every k-length block
+        feat_inds_to_keep_per_block = np.arange(0, feat_inds_to_keep, step=k)
+        feats_to_keep = np.array(feats_to_keep_k_blocks)[ feat_inds_to_keep_per_block ].tolist()
+        
+        return data[feats_to_keep]
