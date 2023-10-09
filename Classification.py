0410ecf8de88b61c4bc20727b9943da29dc19990
Tomi-unh <60202873+Tomi-unh@users.noreply.github.com>
2023-10-08 20:52:58 -0400

    Comments

--- Classification.py
+++ Classification.py
@@ -7,154 +7,119 @@ Created on Sat Oct  7 12:29:04 2023
 import pandas as pd
 import os
 
-
+# Import the getKpindex function from getKPindex.py
 from getKPindex import getKpindex
 
-
-
+# Function to create time conditions for storm data
 def storm_conditions(data_path: str, time_frame: int = 1, unit: str = 'd'):
     '''
-    
+    Parameters:
+        data_path (str): Path to the storm data file.
+        time_frame (int): Time frame for storm conditions.
+        unit (str): Time unit for the time frame (e.g., 'd' for days).
 
-    Parameters
-    ----------
-    data_path : str
-        DESCRIPTION.
-    time_frame : int, optional
-        DESCRIPTION. The default is 2.
-
-    Returns
-    -------
-    None.
-
+    Returns:
+        list: List of time conditions (start_time, end_time).
     '''
+    
+    # Read the storm data from the specified path
     df = pd.read_csv(data_path)
     
-    #convert to datetime
+    # Convert 'time' column to datetime
     df['time'] = pd.to_datetime(df['time'])
     
-    df['time_stop'] = df['time'] + pd.to_timedelta(time_frame, unit = unit)
+    # Calculate start and stop times based on the time frame
+    df['time_stop'] = df['time'] + pd.to_timedelta(time_frame, unit=unit)
+    df['time_start'] = df['time'] - pd.to_timedelta(time_frame, unit=unit)
     
-    df['time_start'] = df['time'] - pd.to_timedelta(time_frame, unit = unit)
-    
-        
+    # Move 'Symh' column to the 4th position
     column_data = df.pop('Symh')
-    
-    # Insert the column at the desired index
     df.insert(4, 'Symh', column_data)
     
-    
+    # Select only 'time_start' and 'time_stop' columns and create a list of time conditions
     selected_columns = ['time_start', 'time_stop']
     time_condition = list(df[selected_columns].itertuples(index=False, name=None))
     
-    
     return time_condition
-    
 
-
-
-def binary_classification( datatype: str = 'Hp30', start_time: str = '2016-01-01',
+# Function for binary classification of Kp index data
+def binary_classification(datatype: str = 'Hp30', start_time: str = '2016-01-01',
                           stop_time: str = '2023-10-06', threshold: int = 3, save_path: str = '../Hp_Data/'):
     '''
-    
+    Parameters:
+        datatype (str): Type of Kp index data (e.g., 'Hp30').
+        start_time (str): Start date for data retrieval.
+        stop_time (str): Stop date for data retrieval.
+        threshold (int): Threshold for binary classification.
+        save_path (str): Path to save the resulting data.
 
-    Parameters
-    ----------
-    datatype : str, optional
-        DESCRIPTION. The default is 'Hp30'.
-    start_time : str, optional
-        DESCRIPTION. The default is '2016-01-01'.
-    stop_time : str, optional
-        DESCRIPTION. The default is '2023-10-06'.
-    threshold : int, optional
-        DESCRIPTION. The default is 3.
-    save_path : str, optional
-        DESCRIPTION. The default is '../Hp_Data/'.
-
-    Returns
-    -------
-    None.
-
+    Returns:
+        None
     '''
     
-    #Get the data 
+    # Get Kp index data using the getKpindex function
+    data = getKpindex(start_time, stop_time, index=datatype)
     
-    data = getKpindex('2016-01-01','2023-10-06',index = datatype)
-    
-    data = {'time': data[0], 'values': data[1]} 
-    
+    # Create a DataFrame from the retrieved data
+    data = {'time': data[0], 'values': data[1]}
     df = pd.DataFrame(data)
     
-    
-    #Create new columns for classifying whether the values go beyond a certain threshold
+    # Create new columns for binary classification based on the threshold
     df[f'above_{threshold}'] = (df['values'] > threshold).astype(int)
     df[f'below_{threshold}'] = (df['values'] < threshold).astype(int)
     
-    #get the time to be in datetime 
+    # Convert the 'time' column to datetime
     df['time'] = pd.to_datetime(df['time'])
     
-    #get the storm data
+    # Get storm data conditions
     storm_path = os.path.join(save_path, 'storm_data.csv')
-    
     conditions = storm_conditions(storm_path)
     
-    #initialize an empty column for storing the storm filtered data
+    # Initialize an empty DataFrame for storing the storm-filtered data
     storm_df = pd.DataFrame(columns=df.columns)
     
-    
+    # Iterate through storm conditions and filter the data
     for start_time, end_time in conditions:
         temp_df = df[(df['time'] >= start_time.tz_localize('UTC')) & (df['time'] <= end_time.tz_localize('UTC'))]
         storm_df = pd.concat([storm_df, temp_df], ignore_index=True)
     
-    #Create folder in parent directory if it doens't exits
-    
+    # Create a folder in the parent directory if it doesn't exist
     if not os.path.exists(save_path):
         os.makedirs(save_path)
         print(f"Folder '{save_path}' created.")
     else:
        print(f"Folder '{save_path}' already exists.")
     
-    
+    # Save the storm-filtered data to a CSV file
     filename = os.path.join(save_path, 'storm_binary_data.csv')
     storm_df.to_csv(filename)
-    
-    
+
+# Function for multiclass classification of Kp index data
 def multiple_classification(datatype: str = 'Hp30', start_time: str = '2016-01-01', 
-                            stop_time: str = '2023-10-06', threshold: tuple = (3,6,9),save_path: str = '../Hp_Data/'):
+                            stop_time: str = '2023-10-06', threshold: tuple = (3, 6, 9), save_path: str = '../Hp_Data/'):
+    '''
+    Parameters:
+        datatype (str): Type of Kp index data (e.g., 'Hp30').
+        start_time (str): Start date for data retrieval.
+        stop_time (str): Stop date for data retrieval.
+        threshold (tuple): Thresholds for multiclass classification.
+        save_path (str): Path to save the resulting data.
+
+    Returns:
+        None
     '''
     
-
-    Parameters
-    ----------
-    datatype : str, optional
-        DESCRIPTION. The default is 'Hp30'.
-    start_time : str, optional
-        DESCRIPTION. The default is '2016-01-01'.
-    stop_time : str, optional
-        DESCRIPTION. The default is '2023-10-06'.
-    threshold : tuple, optional
-        DESCRIPTION. The default is (3,6,9).
-    save_path : str, optional
-        DESCRIPTION. The default is '../Hp_Data/'.
-
-    Returns
-    -------
-    None.
-
-    '''
+    # Get Kp index data using the getKpindex function
+    data = getKpindex(start_time, stop_time, index=datatype)
     
-    #Get the data 
-    data = getKpindex('2016-01-01','2023-10-06',index = datatype)
-    
-    data = {'time': data[0], 'values': data[1]} 
-    
+    # Create a DataFrame from the retrieved data
+    data = {'time': data[0], 'values': data[1]}
     df = pd.DataFrame(data) 
     
-    #define the thresholds 
+    # Define the thresholds
     t1, t2, t3 = threshold
     
-    
-    #Create new columns for classifying whether the values go beyond a certain threshold
+    # Create new columns for multiclass classification based on the thresholds
     df[f'above_{t1}'] = (df['values'] > t1).astype(int)
     df[f'below_{t1}'] = (df['values'] < t1).astype(int)
     
@@ -164,45 +129,41 @@ def multiple_classification(datatype: str = 'Hp30', start_time: str = '2016-01-0
     df[f'above_{t3}'] = (df['values'] > t3).astype(int)
     df[f'below_{t3}'] = (df['values'] < t3).astype(int)
     
-    #get the time to be in datetime 
+    # Convert the 'time' column to datetime
     df['time'] = pd.to_datetime(df['time'])
     
-    #get the storm data
+    # Get storm data conditions
     storm_path = os.path.join(save_path, 'storm_data.csv')
-    
     conditions = storm_conditions(storm_path)
     
-    #initialize an empty column for storing the storm filtered data
+    # Initialize an empty DataFrame for storing the storm-filtered data
     storm_df = pd.DataFrame(columns=df.columns)
     
-    
+    # Iterate through storm conditions and filter the data
     for start_time, end_time in conditions:
-        temp_df = df[(df['time'] >= start_time.tz_localize('UTC')) & (df['time'] <= end_time.tz_localize('UTC'))]
+        temp_df = df[(df['time'] >= start_time) & (df['time'] <= end_time)]
         storm_df = pd.concat([storm_df, temp_df], ignore_index=True)
     
-    
-    
-    #Create folder in parent directory if it doens't exits
-    
+    # Create a folder in the parent directory if it doesn't exist
     if not os.path.exists(save_path):
         os.makedirs(save_path)
         print(f"Folder '{save_path}' created.")
     else:
        print(f"Folder '{save_path}' already exists.")
     
-    
+    # Save the storm-filtered data to a CSV file
     filename = os.path.join(save_path, 'storm_multiclass_data.csv')
     storm_df.to_csv(filename)
-    
-    
-    
+
 if __name__ == "__main__":
     print('Executing both functions')
     
+    # Call the binary_classification and multiple_classification functions
     binary_classification()
     multiple_classification()
     
     print('DONE!!!')
+
     
     
     
--- storm_list.py
+++ storm_list.py
@@ -10,45 +10,37 @@ import os
 import pandas as pd
 import spacepy.pycdf as cdf
 
-
 def storm_list(path, threshold: int = -50, min_spacing: int = 2, freq: int = 5, save_path: str = '../Hp_Data/'):
     '''
-    
+    Extracts storm data from CDF files and saves it as a CSV file.
 
-    Parameters
-    ----------
-    path : TYPE
-        DESCRIPTION.
-    threshold : int, optional
-        DESCRIPTION. The default is 50.
-    min_spacing : int, optional
-        DESCRIPTION. The default is 1.
-    freq : int, optional
-        DESCRIPTION. The default is 5.
+    Parameters:
+        path (str): Path to the directory containing CDF files.
+        threshold (int, optional): Threshold value for detecting storm events. Default is -50.
+        min_spacing (int, optional): Minimum spacing between storm events (in minutes). Default is 2.
+        freq (int, optional): Data frequency in minutes. Default is 5.
+        save_path (str, optional): Path to save the resulting CSV file. Default is '../Hp_Data/'.
 
-    Returns
-    -------
-    None.
-
+    Returns:
+        None.
     '''
-    #calculate the minimum time between the detection of one event and the next.
-    min_spacing = min_spacing*(1440/freq)
-
+    
+    # Calculate the minimum time between the detection of one event and the next.
+    min_spacing = min_spacing * (1440 / freq)  # Convert min_spacing to data points
+    
     storm_time = []
     storm_value = []
     
     for folder in os.listdir(path):
-        for file in os.listdir(os.path.join(path,folder)):
+        for file in os.listdir(os.path.join(path, folder)):
             
-            data = cdf.CDF(os.path.join(path,folder,file))
+            data = cdf.CDF(os.path.join(path, folder, file))
             
             time = data['Epoch'][:]
             symh = data['SYM_H'][:]
         
-        
             prev_value = None
             prev_index = None
-            
             
             for index, value in enumerate(symh):
                 if value <= threshold:
@@ -73,21 +65,20 @@ def storm_list(path, threshold: int = -50, min_spacing: int = 2, freq: int = 5, 
         os.makedirs(save_path)
         print(f"Folder '{save_path}' created.")
     else:
-       print(f"Folder '{save_path}' already exists.")
-    
+        print(f"Folder '{save_path}' already exists.")
     
     filename = os.path.join(save_path, 'storm_data.csv')
     df.to_csv(filename)
-    
+
 if __name__ == "__main__":
     print('Getting Storm Data')
     
-    path = 'D:/OMNI/'
+    path = 'D:/OMNI/'  # Specify the path to your CDF files directory
     
-    storm_list(path = path)
+    storm_list(path=path)
     
     print('DONE!!!')
-    
+
     
     
     
